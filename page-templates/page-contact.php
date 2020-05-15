f73814eef619c740cc17d3fdc780e3987830166a
Johan Borg <johanborg81@hotmail.com>
2020-05-15 14:41:43 +0900

    Clean up

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -11,8 +11,8 @@ get_header();
 if(have_posts()):
     while(have_posts()): the_post(); ?>
     <div class="showcase__header">
+        <?php the_post_thumbnail(); ?>
         <h2><?php the_title(); ?></h2>
-        <h2>Hello from Contact page</h2>
     </div>
     <section class="section">
         <?php the_content(); ?>
@@ -21,3 +21,4 @@ if(have_posts()):
     endwhile;
 endif;
 get_footer();
+
